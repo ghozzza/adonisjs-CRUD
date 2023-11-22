42fa4e8b4a59dff7d12b726718e7a4ba652128b3
ghozzza <ghoza60@gmail.com>
2023-11-22 19:50:46 +0900

    CRUD done

--- ewBhAGQAbwBuAGkAcwAtAHAAcgBvAGoAZQBjAHQAfQA=/start/routes.ts
+++ ewBhAGQAbwBuAGkAcwAtAHAAcgBvAGoAZQBjAHQAfQA=/start/routes.ts
@@ -26,4 +26,8 @@ Route.get('/', async () => {
 
 Route.group(() => {
   Route.post('users', 'UsersController.store')
-})
+  Route.get('users', 'UsersController.index')
+  Route.get('users/:id', 'UsersController.show')
+  Route.put('users/:id', 'UsersController.update')
+  Route.delete('users/:id', 'UsersController.destroy')
+}).prefix('/api/v1')
